418c441ce568734ec4031535e49f91070cd283eb
Agustin Gomez <agusleongomez@gmail.com>
2022-11-15 09:22:18 -0300

    changed static values of app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Series, WeatherData } from './models/weather.module';
 import { WeatherService } from './services/weather.service';
 
 @Component({
@@ -9,11 +10,39 @@ import { WeatherService } from './services/weather.service';
 export class AppComponent implements OnInit {
   constructor(private weatherService: WeatherService) {}
 
+  wheatherData?: Series[];
+  minTemp?: number;
+  maxTemp?: number;
+  humidity?: string;
+  wind?: number;
+
   ngOnInit(): void {
     this.weatherService.getWeatherData('-60,693', '-32,959').subscribe({
       next: (response) => {
-        console.log(response);
+        this.wheatherData = response.dataseries;
+        let [min, max] = this.getTemperatures(this.wheatherData);
+        this.minTemp = min;
+        this.maxTemp = max;
+        this.humidity = this.wheatherData[0].rh2m;
+        this.wind = this.wheatherData[0].wind10m.speed;
+      },
+    });
+  }
+
+  getTemperatures(data: Series[]) {
+    let min = data[0].temp2m;
+    let max = data[0].temp2m;
+
+    for (let i = 1; i < 8; i++) {
+      if (data[i].temp2m > max) {
+        max = data[i].temp2m;
       }
-    })
+
+      if (data[i].temp2m < min) {
+        min = data[i].temp2m;
+      }
+    }
+
+    return [min, max];
   }
 }
